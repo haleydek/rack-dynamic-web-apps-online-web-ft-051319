168adeee220ad6c3f5953df277d02ba30d563613
Haley DeKeyser <hcd09@live.com>
2019-06-20 15:59:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,14 +2,15 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
+    
     if num_1==num_2 && num_2==num_3
-      puts "You Win"
+      resp.write "You Win"
     else
-      puts "You Lose"
+      resp.write "You Lose"
     end
     resp.finish
   end
